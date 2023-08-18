ddff4965e55da6b09a82deeaf2dddacd8f31e200
asmraihan <asmraihanctg14@gmail.com>
2023-08-18 12:03:01 +0600

    banner done

--- main.js
+++ main.js
@@ -16,24 +16,30 @@ document.querySelector('#app').innerHTML = `
           <p>Support 24/7</p>
           </div>
           <div class='lower_menu'>
-            <div class='lower_menu_item'> <i class="fa-solid fa-desktop"></i> <p>PC</p> <i class="fas fa-regular fa-angle-down"></i> </div>
-            <div class='lower_menu_item'> <i class="fa-brands fa-playstation"></i> <p>Playstation</p> <i class="fas fa-regular fa-angle-down"></i> </div>
-            <div class='lower_menu_item'> <i class="fa-brands fa-xbox"></i> <p>Xbox</p> <i class="fas fa-regular fa-angle-down"></i> </div>
-            <div class='lower_menu_item'> <i class="fa-solid fa-swatchbook"></i> <p>Nintendo</p> <i class="fas fa-regular fa-angle-down"></i> </div>
+            <div class='lower_menu_item'> <i class="fa-solid fa-desktop"></i> <p>PC</p> <i class="fa-solid fa-chevron-down down_arrow"></i>  </div>
+            <div class='lower_menu_item'> <i class="fa-brands fa-playstation"></i> <p>Playstation</p> <i class="fa-solid fa-chevron-down down_arrow"></i>  </div>
+            <div class='lower_menu_item'> <i class="fa-brands fa-xbox"></i> <p>Xbox</p> <i class="fa-solid fa-chevron-down down_arrow"></i>  </div>
+            <div class='lower_menu_item'> <i class="fa-solid fa-swatchbook"></i> <p>Nintendo</p> <i class="fa-solid fa-chevron-down down_arrow"></i> </div>
            
-            <div> <i class="fa-solid fa-magnifying-glass"></i> </div>
+            <div class='search_icon'> <i class="fa-solid fa-magnifying-glass"></i> </div>
           </div>
         </div>
         <div class='right_icons'>
           <p><i class="fa-solid fa-cart-arrow-down"></i></p>
-          <p><i class="fa-regular fa-user"></i></p>
+          <div class='user_icon'><i class="fa-regular fa-user"></i></div>
         </div>
       </nav>
       <div class='banner'>
-      
+          <div class='banner_info'>
+              <p class='time_tag'>In 15 days</p>
+              <h3 class='banner_title'>Mandalorian Early Access + Digital Deluxe Edition</h3>
+              <div class='pricing_info'>
+              <span class='discount_tag'>-25%</span> <p class='price'>74.99€</p>
+          </div>
+        </div>
       </div>
   </div>
 `
 
 // setupCounter(document.querySelector('#counter'))
-{/* <img class='banner_image' src="${banner}" alt="banner"/> */}
+{/* <img class='banner_image' src="${banner}" alt="banner"/> */ }
